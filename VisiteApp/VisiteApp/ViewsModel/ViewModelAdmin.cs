11d470b37aa34c21998f75eeae91115526f373c8
PauchardThomas <thomaspauch@hotmail.fr>
2017-05-10 16:55:54 +0200

    Ajout des commandes

--- VisiteApp/VisiteApp/ViewsModel/ViewModelAdmin.cs
+++ VisiteApp/VisiteApp/ViewsModel/ViewModelAdmin.cs
@@ -10,24 +10,55 @@ namespace VisiteApp.ViewsModel
     public class ViewModelAdmin : Observable
     {
         #region Fields
-       
+        private ICommand _ModifierProduit;
+        private ICommand _Synchro;
+        private ICommand _NewProduit;
+        private ICommand _NewVisite;
         #endregion
 
         #region Properties
-       
+        public ICommand ModifierProduit { get { return _ModifierProduit; } }
+        public ICommand Synchro { get { return _Synchro; } }
+        public ICommand NewProduit { get { return _NewProduit; } }
+        public ICommand NewVisite { get { return _ModifierProduit; } }
         #endregion
 
         #region Constructor
         public ViewModelAdmin(INavigation nav)
         {
-            
+            this._ModifierProduit = new Command(ModifierExecuted);
+            this._Synchro = new Command(SynchroExecuted);
+            this._NewProduit = new Command(NewProduitExecuted);
+            this._NewVisite = new Command(NewVisiteExecuted);
+        }
+
+        private void NewVisiteExecuted(object obj)
+        {
+           // Vers page Visite
+        }
+
+        private void NewProduitExecuted(object obj)
+        {
+           // Vers page Produit
+        }
+
+        private void SynchroExecuted(object obj)
+        {
+           // Récupération des visites non synchros
+
+          // Appel du WS pour synchronisation
+        }
+
+        private void ModifierExecuted(object obj)
+        {
+            // Vers Page produit
         }
 
         #endregion
 
         #region Methods
 
-        
+
 
         #endregion
     }
