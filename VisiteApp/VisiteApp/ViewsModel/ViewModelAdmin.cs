c565a859740ebcbb15ceb7eeac099205283855f5
kevin <k.alonso@iia-laval.fr>
2017-05-10 16:42:11 +0200

    Add Binding and delete Project windows

--- VisiteApp/VisiteApp/ViewsModel/ViewModelAdmin.cs
+++ VisiteApp/VisiteApp/ViewsModel/ViewModelAdmin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 using VisiteApp.Core;
+using VisiteApp.Views;
 using Xamarin.Forms;
 
 namespace VisiteApp.ViewsModel
@@ -10,11 +11,23 @@ namespace VisiteApp.ViewsModel
     public class ViewModelAdmin : Observable
     {
         #region Fields
-       
+        private Admin _Admin;
         #endregion
 
         #region Properties
-       
+
+        public Admin Admin
+        {
+            get { return _Admin; }
+            set
+            {
+                OnPropertyChanging(nameof(Admin));
+                _Admin = value;
+                OnPropertyChanged(nameof(Admin));
+
+            }
+        }
+
         #endregion
 
         #region Constructor
