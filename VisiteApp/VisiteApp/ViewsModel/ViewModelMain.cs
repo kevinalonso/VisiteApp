6da34d368e0ae870a847a76d59359e427be0b73f
kevin <k.alonso@iia-laval.fr>
2017-05-10 15:14:33 +0200

    ViewsModel

---
+++ VisiteApp/VisiteApp/ViewsModel/ViewModelMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisiteApp.ViewsModel
+{
+    public class ViewModelMain
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+        public ViewModelMain()
+        {
+
+        }
+
+        #endregion
+    }
+}
