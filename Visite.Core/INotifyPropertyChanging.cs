87991b50ff537610385dd1a1b521fadbff40d619
kevin <k.alonso@iia-laval.fr>
2017-05-10 15:01:31 +0200

    Start

---
+++ Visite.Core/INotifyPropertyChanging.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Visite.App.Core
+{
+    public interface INotifyPropertyChanging
+    {
+        //
+        // Résumé :
+        //     Se produit en cas de modification d'une valeur de propriété.
+        event PropertyChangingEventHandler PropertyChanging;
+    }
+}
