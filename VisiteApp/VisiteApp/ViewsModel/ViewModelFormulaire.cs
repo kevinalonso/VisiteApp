43610194966c7008220669d9005d76d8123333ff
kevin <k.alonso@iia-laval.fr>
2017-05-12 11:03:04 +0200

    Picker formulaire OK

--- VisiteApp/VisiteApp/ViewsModel/ViewModelFormulaire.cs
+++ VisiteApp/VisiteApp/ViewsModel/ViewModelFormulaire.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
+using VisiteApp.Core;
+using VisiteApp.Data;
+using VisiteApp.Entity;
 using VisiteApp.Views;
 using Xamarin.Forms;
 
 namespace VisiteApp.ViewsModel
 {
-    public class ViewModelFormulaire
+    public class ViewModelFormulaire : Observable
     {
         #region Fields
         private INavigation _Navigation;
         private Formulaire _Formulaire;
+
+        private Produit _Produit;
+
+        private ICommand _Valider;
+
+        private ObservableCollection<Visite> _Visites;
         #endregion
 
         #region Properties
-
-        #endregion
-
-        #region Constructor
-        public ViewModelFormulaire(INavigation nav)
-        {
-            _Navigation = nav;
-        }
-
         public INavigation Navigation
         {
             get
@@ -49,9 +51,65 @@ namespace VisiteApp.ViewsModel
             }
         }
 
+        public ICommand Valider { get { return _Valider; } }
+
+        public ObservableCollection<Visite> Visites
+        {
+            get { return _Visites; }
+            private set
+            {
+                OnPropertyChanging(nameof(Visites));
+                _Visites = value;
+                OnPropertyChanged(nameof(Visites));
+            }
+        }
+
+        public Produit ProduitEntity
+        {
+            get { return _Produit; }
+            set
+            {
+                OnPropertyChanging(nameof(ProduitEntity));
+                _Produit = value;
+                OnPropertyChanged(nameof(ProduitEntity));
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public ViewModelFormulaire(INavigation nav)
+        {
+            _Navigation = nav;
+            Visites = new ObservableCollection<Visite>();
+            GetVisite();
+           
+            //Command
+            this._Valider = new Command(ValiderExecuted);
+        }
+
+
+
         #endregion
 
         #region Methods
+
+        private void ValiderExecuted(object obj)
+        {
+
+        }
+
+        private void GetVisite()
+        {
+            //Récupération en base de données locale les visite pour le formulaire
+            DBVisite db = new DBVisite();
+            IEnumerable<Visite> vs = db.getAll();
+
+            foreach(Visite item in vs)
+            {
+                Visites.Add(item);
+            }
+        }
 
         #endregion
     }
